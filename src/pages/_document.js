4ba7bb72721bad0c32e57136d64bacc3f3cc2b09
felpzk <mar.cos11@outlook.com>
2023-05-04 14:17:00 -0300

    finalizado

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="pt-br" >
-      <Head>
-        <title>Alura - Cases</title>
-      </Head>
+      <Head/>
       <body>
         <Main />
         <NextScript />
